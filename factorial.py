0d222a72342d51f01e616f7ad5ae1389636b447d
stormybeat <128640313+stormybeat@users.noreply.github.com>
2024-03-01 19:12:25 -0500

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,25 +1,10 @@
 import sys
 sys.set_int_max_str_digits(0) # let python convert arbitrarily long numbers into strings. 
 
-'''
-to do:
-    make an split list function to split the number to make a factorial of in half.
-    make them all use different threads.
-'''
-
-def _em2generateLists(generationInput):
-    generationOutput = []
-    for i in range(1, generationInput + 1):
-        generationOutput.append(i)
-    return generationOutput
 
 
-    
-def _em2factorial(listInput):
-    answer = 1
-    for i in listInput:
+def _factorial(num):
+    answer = num
+    for i in range(2,num):
         answer *= (i)
     return answer
-
-
-print(_em2generateLists(10))

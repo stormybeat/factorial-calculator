fa89b93fa3af67d2f1127b4c3be9885a8d678daf
stormybeat <128640313+stormybeat@users.noreply.github.com>
2024-03-01 19:56:54 -0500

    Add code for getting execution time. Comes with free example.

--- factorial.py
+++ factorial.py
@@ -1,4 +1,5 @@
 import sys
+import timeit
 sys.set_int_max_str_digits(0) # let python convert arbitrarily long numbers into strings. 
 
 
@@ -8,3 +9,20 @@ def _factorial(num):
     for i in range(2,num):
         answer *= (i)
     return answer
+
+
+num = 100000
+
+
+file = open(f'./computed_factorials/{num}_factorial.txt', 'w')
+file.write(str(_factorial(num)))
+file.close()
+
+
+
+'''
+Code for timing the execution time. 
+'''
+# times_run = 1
+# result = timeit.timeit(stmt='_factorial(num)', globals=globals(), number = times_run)
+# print(f'CPU Execution time: {result/times_run} seconds')
